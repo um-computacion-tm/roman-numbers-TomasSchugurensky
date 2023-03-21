31b2e5a25cfdc6503e70ca6525b56782f8044007
Tomas Schugurensky <tomas@pop-os.localdomain>
2023-03-21 12:16:57 -0300

    First commit

---
+++ decimal_to_roman.py
@@ -0,0 +1,38 @@
+def decimal_to_roman(decimal):
+   
+    if decimal <= 3: 
+        return "I" * decimal
+    elif decimal == 5:
+        return 'V' 
+    elif decimal == 9:
+        return 'IX'
+    elif decimal == 10:
+        return "X"  
+
+
+
+import unittest
+class TestDecimalToRoman (unittest.TestCase):
+    def test_uno(self):
+        resultado = decimal_to_roman(1)
+        self.assertEqual(resultado, 'I')
+    def test_cinco(self):
+        resultado = decimal_to_roman(5)
+        self.assertEqual(resultado, 'V')
+    def test_dos(self):
+        resultado = decimal_to_roman(2)
+        self.assertEqual(resultado,'II')
+    def test_tres(self):
+        resultado = decimal_to_roman(3)
+        self.assertEqual (resultado, 'III')
+    def test_seis(self):
+        resultado = decimal_to_roman(6)
+        self.assertEqual(resultado, 'VI')
+    def test_diez(self):
+        resultado = decimal_to_roman(10)
+        self.assertEqual(resultado, 'X')
+    def test_nueve(self):
+        resultado = decimal_to_roman(9)
+        self.assertEqual(resultado, 'IX')
+if __name__ == "__main__":
+    unittest.main()
